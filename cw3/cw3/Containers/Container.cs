9943e6d75e9a9fbcb06332008f4a3df09f882708
Franciszek Gozdecki <s27379@pjwstk.edu.pl>
2024-03-19 11:36:23 +0100

    Added Container abstract class: * Implements IContainer interface (Empty(), Load() methods)
    Added LiquidContainer class:
    * Implements IHazardNotifier (Notify() method)
    
    Added OverfillException

---
+++ cw3/cw3/Containers/Container.cs
@@ -0,0 +1,61 @@
+﻿namespace cw3.Containers;
+
+abstract class Container : IContainer {
+    public double LoadMass { get; set; }
+    protected string Serial { get; set; }
+    protected char? ContType { get; set; }
+    protected double Height { get; set; }
+    protected double ContainerWeight { get; set; }
+    protected double Depth { get; set; }
+    public double MaxLoad { get; set; }
+
+    public Container(double loadMass, double height, double containerWeight, double depth,
+        double maxLoad) {
+        LoadMass = loadMass;
+        Height = height;
+        ContainerWeight = containerWeight;
+        Depth = depth;
+        Serial = CreateSerial();
+        MaxLoad = maxLoad;
+    }
+
+    public void Empty() {
+        LoadMass = 0;
+    }
+
+    public virtual void Load(double loadMass) {
+        { if (loadMass > MaxLoad)
+          { throw new OverfillException("Load is too large: " + loadMass.ToString()); }
+
+          LoadMass = loadMass; }
+    }
+
+    string CreateSerial() {
+        return "KON-" + ContType + "-" + IContainer._id++;
+    }
+}
+
+class LiquidContainer : Container, IHazardNotifier {
+    public LiquidContainer(double loadMass, double height, double containerWeight, double depth, double maxLoad) : base(
+        loadMass, height, containerWeight, depth, maxLoad) {
+        base.ContType = 'L';
+    }
+
+    public void Load(double loadMass, bool isHazardous) {
+        if (isHazardous)
+        { if (loadMass > MaxLoad / 2)
+          { base.Load(loadMass / 2); } }
+
+        base.Load(loadMass);
+    }
+}
+
+internal interface IHazardNotifier {
+    void Notify(string message, string serial) {
+        Console.WriteLine(message + ": " + serial);
+    }
+}
+
+internal class OverfillException : Exception {
+    public OverfillException(string message) : base(message) { }
+}
---
+++ cw3/cw3/IContainer.cs
@@ -0,0 +1,15 @@
+﻿using cw3.Containers;
+
+namespace cw3; 
+
+public interface IContainer {
+    
+
+    static int _id = 0;
+    double MaxLoad { get; set; } 
+    double LoadMass { get; set; }
+
+
+    void Empty();
+    void Load(double loadMass);
+}
